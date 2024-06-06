b46b8f52dba3240492ad00a95b1815500f2cadcd
mammadkhafan <sharifrazavian@gmail.com>
2024-06-06 19:14:19 +0430

    =>packages created=>

--- src/Controllers/SignUpController.java
+++ src/Controllers/SignUpController.java
@@ -1,4 +1,4 @@
-package MainPackage;
+package Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("loginPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml_files/loginPage.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
