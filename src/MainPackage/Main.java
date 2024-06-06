387ea849a15172a67edab038fa1ec20b06d74015
Abolfazl.A <abolfazlabolfazlamani1384@gmail.com>
2024-06-06 20:17:30 +0330

    => test =>

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml_files/loginPage.fxml"));
+        // Parent root = FXMLLoader.load(getClass().getResource("/FXMLFiles/loginPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/FXMLFiles/SignUp.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
